a3d34a4fa6655361a32225fa9403294a87651f05
Lucas <lucas.7795.la@gmail.com>
2023-05-19 20:03:36 -0300

    Aplicando filtro de autorização, monitamento e controle

---
+++ src/br/com/alura/gerenciador/servlet/MonitoramentoFilter.java
@@ -0,0 +1,49 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/entrada")
+public class MonitoramentoFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public MonitoramentoFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		System.out.println("MonitoramentoFilter");
+		Long antes = System.currentTimeMillis();
+		String acao =  request.getParameter("acao");
+		chain.doFilter(request, response);
+		Long depois = System.currentTimeMillis();
+		System.out.println("Tempo de execução da acao " + acao + " -> " + (depois - antes));
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
